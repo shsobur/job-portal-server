d0fb818612a7afafb2db03d2a347a99d1456b172
shsobur <shsoburhossen951@gmail.com>
2025-06-01 12:19:27 +0600

    Updated user role route stystem

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 app.use(cors());
 app.use(express.json());
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,7 +31,27 @@ async function run() {
 
     // DB all collections__
     const jobsCollections = client.db("jobPortal").collection("jobs");
+    const usersCollections = client.db("jobPortal").collection("users");
 
+    // Post use data__
+    app.post("/user-role-data", async (req, res) => {
+      try {
+        const user = req.body;
+        const isExist = await usersCollections.findOne({userEmail: req.body.userEmail});
+
+        if(isExist) {
+          return res.status(200).json({ message: "User already exists" });
+        }
+
+        const result = await usersCollections.insertOne(user);
+        res.status(201).send(result);
+      } catch (error) {
+        console.error("Error inserting user role data:", error);
+        res.status(500).send({ error: "Failed to insert user role data"});
+      }
+    });
+
+    // Post job circular__
     app.post("/new-job-circular", async (req, res) => {
       try {
         const jobCircular = req.body;
@@ -44,16 +63,7 @@ async function run() {
       }
     });
 
-
-
-
-
-
-
-
-
-
-
+    // DB work END__
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
